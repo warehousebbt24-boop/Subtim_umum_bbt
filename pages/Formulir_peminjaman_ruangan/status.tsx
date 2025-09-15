336b6fd6b09634e0b69a56a18957b3e5b17e4e77
warehousebbt24-boop <warehousebbt24@gmail.com>
2025-09-15 11:02:31 +0700

    Update status.tsx

--- pages/Formulir_peminjaman_ruangan/status.tsx
+++ pages/Formulir_peminjaman_ruangan/status.tsx
@@ -45,8 +45,10 @@ export default function CekStatus() {
         return "bg-green-100 text-green-800 border-green-200";
       case "pending":
         return "bg-yellow-100 text-yellow-800 border-yellow-200";
-      default:
-        return "bg-gray-100 text-gray-600 border-gray-200";
+       case "selesai":
+      return "bg-blue-100 text-blue-800 border-blue-200";
+    default:
+      return "bg-gray-100 text-gray-600 border-gray-200";
     }
   };
 
@@ -56,6 +58,8 @@ export default function CekStatus() {
         return "Disetujui";
       case "pending":
         return "Menunggu";
+      case "selesai":
+        return"selesai";
       default:
         return "Ditolak";
     }
