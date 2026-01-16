0c2c545ac5744515415d2f903cdd7c7332dce3bf
warehousebbt24-boop <warehousebbt24@gmail.com>
2026-01-16 11:32:49 +0000

    option no pol

--- pages/admin/Peminjaman_kendaraan/dashboard.tsx
+++ pages/admin/Peminjaman_kendaraan/dashboard.tsx
@@ -220,7 +220,7 @@ export default function AdminDashboard() {
                               <select
                                 value={no_pol}
                                 onChange={(e) => handleLocalUpdate(row.id, "no_pol", e.target.value)}
-                                className="border border-gray-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full bg-white disabled:bg-gray-50 disabled:text-gray-400"
+                                className="min-w-max border border-gray-300 rounded-lg px-3 py-2 text-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white disabled:bg-gray-50 disabled:text-gray-400"
                                 disabled={row.accepted}
                               >
                                 <option value="">Pilih No. Pol</option>
